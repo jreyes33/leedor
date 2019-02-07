ff50ca663faac08506b6cf24cccc50332def1868
Jonathan Reyes <j@jreyes.org>
2019-02-06 23:27:46 -0500

    Remove some comments

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -7,8 +7,6 @@ mod xml;
 use cfg_if::cfg_if;
 
 cfg_if! {
-    // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-    // allocator.
     if #[cfg(feature = "wee_alloc")] {
         #[global_allocator]
         static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
